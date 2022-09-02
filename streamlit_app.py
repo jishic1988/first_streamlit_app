b26d017f4ffd25564790fd5239983387355486a0
Jishi C <112691778+jishic1988@users.noreply.github.com>
2022-09-02 14:41:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Favourites')
-streamlit.text('\N{U+1F963}Omega3 and Blueberry oatmeal')
+streamlit.text('\N{bowl with spoon}Omega3 and Blueberry oatmeal')
 streamlit.text('Kale Spinach and Rocket smoothie')
 streamlit.text('Hard Boiled free range egg')
