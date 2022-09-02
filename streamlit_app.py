ba8615356704c012d43d4056485287dca7d54986
Jishi C <112691778+jishic1988@users.noreply.github.com>
2022-09-02 14:45:25 +0530

    Update streamlit_app.py
    Added avocado

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Favourites')
 streamlit.text('\N{bowl with spoon}Omega3 and Blueberry oatmeal')
-streamlit.text('Kale Spinach and Rocket smoothie')
-streamlit.text('Hard Boiled free range egg')
+streamlit.text('\N{leafy green}Kale, Spinach and Rocket smoothie')
+streamlit.text('\N{chicken}Hard Boiled free range egg')
+streamlit.text('\N{avocado}\N{bread}Avocado Toast')
