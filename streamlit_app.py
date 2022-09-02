5000489f18dafeb441c3ed238af89b1f594d7890
Jishi C <112691778+jishic1988@users.noreply.github.com>
2022-09-02 14:46:48 +0530

    Update streamlit_app.py
    New header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ streamlit.text('\N{bowl with spoon}Omega3 and Blueberry oatmeal')
 streamlit.text('\N{leafy green}Kale, Spinach and Rocket smoothie')
 streamlit.text('\N{chicken}Hard Boiled free range egg')
 streamlit.text('\N{avocado}\N{bread}Avocado Toast')
+
+
+streamlit.header('\N{banana}\N{mango}Build your own fruit smoothie\N{kiwi}\N{grape}')
